b6f8aa0f899897dba562c3bf8bc9f5ff351c6239
林柏毅 <22913088h@gmail.com>
2024-05-12 20:37:53 +0800

    0512

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,13 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
+    // Import the BoM for the Firebase platform
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     //facebook
     implementation ("com.facebook.android:facebook-android-sdk:8.x")
     // Retrofit
@@ -76,6 +83,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     //儲存用
     implementation ("com.google.code.gson:gson:2.10.1")
+    implementation(libs.firebase.auth)
     //camera
     val cameraxVersion = "1.3.3"
     implementation ("androidx.camera:camera-core:${cameraxVersion}")
