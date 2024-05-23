861a60388c2d7b221f2bc38f966a97c9cd8f454c
林柏毅 <22913088h@gmail.com>
2024-05-23 21:10:45 +0800

    0523

---
+++ app/src/main/java/com/example/ripcurrent/page/EditPhotoInformationPage.kt
@@ -0,0 +1,16 @@
+package com.example.ripcurrent.page
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun EditPhotoInformationPage(modifier: Modifier, navController: NavHostController) {
+
+}
+@SuppressLint("MissingPermission")
+@Composable
+fun LocationScreen(){
+
+}
